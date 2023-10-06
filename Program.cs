dbce64d7c7e1dc57797add357c2bab89d89c211a
janimos <olgerts.grudulis@gmail.com>
2023-10-06 12:14:24 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace F1_Schedule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("F1 Scedule optimization");
+        }
+    }
+}
